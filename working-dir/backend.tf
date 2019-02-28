a4e66220d3d2ffed8be3613d9b93429164b164ff
KipsasJaujoj <vaidotasslavinskas@gmail.com>
2019-02-28 19:43:48 +0200

    topic 3 once again

--- working-dir/backend.tf
+++ working-dir/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "change-me"
+    bucket  = "vsrandombucketname"
     key     = "workshop/all-in-one/terraform.tfstate"
     region  = "eu-central-1"
     encrypt = true
